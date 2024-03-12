d5dffe5a2fc1975afc8f4e6b622128a66af9afc2
Jiyeong <0106ymk@naver.com>
2024-03-13 00:47:08 +0900

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
   app.use(
     createProxyMiddleware({
-      target: "http://172.28.24.85:8080",
+      target: "YOUR_PORT",
       changeOrigin: true,
     })
   );
